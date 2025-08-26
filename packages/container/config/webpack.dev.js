9445412ea9d943f06ef7c6b5d991a389ada6b085
Pankaj Boruah <pankajboruah28.pb@gmail.com>
2025-08-26 18:32:45 +0530

    Added container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,30 +1,26 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common.js");
 const packageJson = require("../package.json");
 
 const devConfig = {
-  mode: "development",
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: "index.html",
-    },
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "container",
-      filename: "remoteEntry.js",
-      remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
-      },
-      shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+	mode: "development",
+	devServer: {
+		port: 8080,
+		historyApiFallback: {
+			index: "index.html",
+		},
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "container",
+			filename: "remoteEntry.js",
+			remotes: {
+				marketing: "marketing@http://localhost:8081/remoteEntry.js",
+			},
+			shared: packageJson.dependencies,
+		}),
+	],
 };
 
 module.exports = merge(commonConfig, devConfig);
---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,31 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common.js");
+const packageJson = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+	mode: "production",
+	output: {
+		filename: "[name].[contenthash].js",
+	},
+	devServer: {
+		port: 8080,
+		historyApiFallback: {
+			index: "index.html",
+		},
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "container",
+			filename: "remoteEntry.js",
+			remotes: {
+				marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+			},
+			shared: packageJson.dependencies,
+		}),
+	],
+};
+
+module.exports = merge(commonConfig, prodConfig);
