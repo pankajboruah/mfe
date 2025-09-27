cd00faa2ca4728786972ecf4766c84eb6c460eff
Pankaj Boruah <pankajboruah28.pb@gmail.com>
2025-09-27 20:10:41 +0530

    added dashboard

---
+++ packages/container/src/components/DashboardApp.js
@@ -0,0 +1,12 @@
+import React, { useRef, useEffect } from "react";
+import { mount as dashboardMount } from "dashboard/DashboardApp";
+
+export default () => {
+	const ref = useRef(null);
+
+	useEffect(() => {
+		dashboardMount(ref.current);
+	}, []);
+
+	return <div ref={ref} />;
+};
